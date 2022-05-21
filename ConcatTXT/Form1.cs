dd5220065084fc9671b129deb98f554577eb5fd3
Dantaleon <105968793+Dantaleon@users.noreply.github.com>
2022-05-22 02:11:12 +0300

    Functionality stage 3 (final)
    Выполнены дополнительные условия (require) и условия со *

--- ConcatTXT/Form1.cs
+++ ConcatTXT/Form1.cs
@@ -17,6 +17,8 @@ namespace ConcatTXT
         private string rootPath; // для пути до папки
         private List<string> listPaths = new List<string>(); // для путей txt файлов
         private string rootPathOut; // для пути до выходного файла
+        private int[ , ] reqMatrix; // 2-мерная матрица для зависимостей
+        private bool isCycle = false; // флаг для циклической зависимости
 
         public Form1()
         {
@@ -42,7 +44,7 @@ namespace ConcatTXT
                 btnClearAll.Enabled = true;
                 btnStart.Enabled = false;
 
-                // вызываем рекурсивную функцию
+                // вызываем рекурсивную функцию по поиску текстовиков
                 FindTxtFiles(rootPath, listPaths);
             }
             else
@@ -54,15 +56,61 @@ namespace ConcatTXT
             if (listPaths.Count > 0)
             {
                 richTextBoxLog.Text += "\nТекстовые файлы найдены.";
+
                 // сортируем лист по алфавиту (в листе указаны полные пути, начиная с диска)
                 listPaths.Sort();
 
-                // выводим массив в текстовое поле
-                foreach (string str in listPaths)
+                // инициализируем квадратную матрицу на кол-во путей
+                reqMatrix = new int[listPaths.Count, listPaths.Count];
+
+                // формируем матрицу зависимостей
+                FillReqMatrix(listPaths,reqMatrix);
+
+                // проверяем матрицу на наличие циклических зависимостей
+                for (int i = 0; i < reqMatrix.GetLength(0); i++)
                 {
-                    richTextBoxOut.Text += str + "\n";
+                    for (int j = 0; j < reqMatrix.GetLength(1); j++)
+                    {
+                        // если единицы зеркально расположены (диагональ считается)
+                        if (reqMatrix[i,j] == reqMatrix[j, i] && reqMatrix[i,j] == 1)
+                        {
+                            isCycle = true;
+                            richTextBoxOut.Text += "\n Циклическа зависимость: \n"
+                                                   + listPaths.ElementAt(i) + "\n"
+                                                   + listPaths.ElementAt(j);
+                        }
+                    }
                 }
-                // вызываем функцию для объединения файлов в один файл
+
+                // если не нашли циклическую зависимость
+                if (isCycle == false)
+                {
+                    // сортируем список путей
+                    // пробегаем по матрице
+                    for (int i = 0; i < reqMatrix.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < reqMatrix.GetLength(1); j++)
+                        {
+                            // если нашли зависимость
+                            if (reqMatrix[i,j] == 1)
+                            {
+                                // и если зависимый элемент выше главного (индекс меньше)
+                                if(i < j)
+                                {
+                                    // перемещаем его ниже главного
+                                    string temp = listPaths[i];
+                                    listPaths.RemoveAt(i);
+                                    listPaths.Insert(j, temp);
+                                }
+                            }
+                        }
+                    }
+                }
+                foreach(string path in listPaths)
+                {
+                    richTextBoxOut.Text += "\n" + path;
+                }
+                // вызываем метод для объединения файлов в один файл
                 ConcatTxtFiles(listPaths, rootPath);
             }
             else
@@ -74,7 +122,7 @@ namespace ConcatTXT
                                        + "\nПуть: " + rootPathOut;
 
         }
-        // функция принимает на вход путь до папки, в которой ищем текстовики,
+        // метод принимает на вход путь до папки, в которой ищем текстовики,
         // и лист для записи путей
         private void FindTxtFiles(string path, List<string> listOut)
         {
@@ -90,7 +138,7 @@ namespace ConcatTXT
             }
         }
 
-        // функция объединяет найденные ранее файлы в один файл
+        // метод объединяет найденные ранее файлы в один файл
         private void ConcatTxtFiles(List<string> txtPaths, string pathOut)
         {
             // создаем писаря для нового файла по пути пользователя
@@ -145,5 +193,53 @@ namespace ConcatTXT
         {
             this.Close();
         }
+
+        // метод заполняет матрицу зависимостей
+        private void FillReqMatrix(List<string> paths, int[,] matrix)
+        {
+            // для каждого пути
+            for (int i = 0; i < paths.Count; i++)
+            {
+                // создаем лист для require строк
+                List<string> rStr = new List<string>();
+                // находим строки и заполняем лист
+                // для каждой строки в файле текущего пути
+                foreach (string line in File.ReadAllLines(paths[i]))
+                {
+                    // если находится строка со словом require
+                    if (line.Contains("require"))
+                    {
+                        // определяем границы подстроки для line
+                        int strInit = line.IndexOf('"') + 1;
+                        int strEnd = line.IndexOf('"', strInit);
+                        // выделяем подстроку, соединяя с корневым каталогом и расширением
+                        string strAdd = rootPath + "\\" + line.Substring(strInit, strEnd - strInit) + ".txt";
+                        // добавляем в лист для require
+                        rStr.Add(strAdd);
+                    }
+                }
+                // если удалось собрать строки require
+                if(rStr.Count > 0)
+                {
+                    // для каждой собранной строки
+                    for (int n = 0; n < rStr.Count; n++)
+                    {
+                        // пробегаем по всем путям
+                        for (int k = 0; k < paths.Count; k++)
+                        {
+                            // и если находим совпадение
+                            if(rStr[n].Equals(paths[k]))
+                            {
+                                // записываем в матрицу 1 на месте k-того столбца (строка определяется i)
+                                matrix[i, k] = 1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+
+        
     }
 }
