581ffc72f882352b5e68d1fdcd3a043723f219f4
Dantaleon <105968793+Dantaleon@users.noreply.github.com>
2022-05-21 19:29:46 +0300

    Функциональность стадии 1
    Выполнены требования для самого базового набора функций (без дополнительно и *)

--- ConcatTXT/Form1.Designer.cs
+++ ConcatTXT/Form1.Designer.cs
@@ -30,13 +30,14 @@ namespace ConcatTXT
         private void InitializeComponent()
         {
             this.panelInput = new System.Windows.Forms.Panel();
-            this.panelOutput = new System.Windows.Forms.Panel();
-            this.labelPath = new System.Windows.Forms.Label();
-            this.textBoxFPath = new System.Windows.Forms.TextBox();
             this.btnStart = new System.Windows.Forms.Button();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.labelPath = new System.Windows.Forms.Label();
+            this.panelOutput = new System.Windows.Forms.Panel();
+            this.richTextBoxLog = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxOut = new System.Windows.Forms.RichTextBox();
             this.btnClearAll = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
-            this.richTextBoxOut = new System.Windows.Forms.RichTextBox();
             this.panelInput.SuspendLayout();
             this.panelOutput.SuspendLayout();
             this.SuspendLayout();
@@ -44,20 +45,29 @@ namespace ConcatTXT
             // panelInput
             // 
             this.panelInput.Controls.Add(this.btnStart);
-            this.panelInput.Controls.Add(this.textBoxFPath);
+            this.panelInput.Controls.Add(this.textBoxPath);
             this.panelInput.Controls.Add(this.labelPath);
             this.panelInput.Location = new System.Drawing.Point(3, 12);
             this.panelInput.Name = "panelInput";
             this.panelInput.Size = new System.Drawing.Size(317, 145);
             this.panelInput.TabIndex = 0;
             // 
-            // panelOutput
+            // btnStart
             // 
-            this.panelOutput.Controls.Add(this.richTextBoxOut);
-            this.panelOutput.Location = new System.Drawing.Point(326, 12);
-            this.panelOutput.Name = "panelOutput";
-            this.panelOutput.Size = new System.Drawing.Size(518, 406);
-            this.panelOutput.TabIndex = 1;
+            this.btnStart.Location = new System.Drawing.Point(226, 59);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(88, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Запустить";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(3, 30);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(311, 23);
+            this.textBoxPath.TabIndex = 1;
             // 
             // labelPath
             // 
@@ -68,30 +78,43 @@ namespace ConcatTXT
             this.labelPath.TabIndex = 0;
             this.labelPath.Text = "Укажите путь к папке :";
             // 
-            // textBoxFPath
+            // panelOutput
             // 
-            this.textBoxFPath.Location = new System.Drawing.Point(3, 30);
-            this.textBoxFPath.Name = "textBoxFPath";
-            this.textBoxFPath.Size = new System.Drawing.Size(311, 23);
-            this.textBoxFPath.TabIndex = 1;
+            this.panelOutput.Controls.Add(this.richTextBoxLog);
+            this.panelOutput.Controls.Add(this.richTextBoxOut);
+            this.panelOutput.Location = new System.Drawing.Point(326, 12);
+            this.panelOutput.Name = "panelOutput";
+            this.panelOutput.Size = new System.Drawing.Size(518, 406);
+            this.panelOutput.TabIndex = 1;
             // 
-            // btnStart
+            // richTextBoxLog
             // 
-            this.btnStart.Location = new System.Drawing.Point(226, 59);
-            this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(88, 23);
-            this.btnStart.TabIndex = 2;
-            this.btnStart.Text = "Запустить";
-            this.btnStart.UseVisualStyleBackColor = true;
+            this.richTextBoxLog.Location = new System.Drawing.Point(3, 280);
+            this.richTextBoxLog.Name = "richTextBoxLog";
+            this.richTextBoxLog.ReadOnly = true;
+            this.richTextBoxLog.Size = new System.Drawing.Size(512, 123);
+            this.richTextBoxLog.TabIndex = 1;
+            this.richTextBoxLog.Text = "Состояние :";
+            // 
+            // richTextBoxOut
+            // 
+            this.richTextBoxOut.Location = new System.Drawing.Point(3, 3);
+            this.richTextBoxOut.Name = "richTextBoxOut";
+            this.richTextBoxOut.ReadOnly = true;
+            this.richTextBoxOut.Size = new System.Drawing.Size(512, 271);
+            this.richTextBoxOut.TabIndex = 0;
+            this.richTextBoxOut.Text = "";
             // 
             // btnClearAll
             // 
+            this.btnClearAll.Enabled = false;
             this.btnClearAll.Location = new System.Drawing.Point(207, 395);
             this.btnClearAll.Name = "btnClearAll";
             this.btnClearAll.Size = new System.Drawing.Size(110, 23);
             this.btnClearAll.TabIndex = 2;
             this.btnClearAll.Text = "Сбросить все";
             this.btnClearAll.UseVisualStyleBackColor = true;
+            this.btnClearAll.Click += new System.EventHandler(this.btnClearAll_Click);
             // 
             // btnExit
             // 
@@ -101,14 +124,7 @@ namespace ConcatTXT
             this.btnExit.TabIndex = 3;
             this.btnExit.Text = "Выход";
             this.btnExit.UseVisualStyleBackColor = true;
-            // 
-            // richTextBoxOut
-            // 
-            this.richTextBoxOut.Location = new System.Drawing.Point(3, 3);
-            this.richTextBoxOut.Name = "richTextBoxOut";
-            this.richTextBoxOut.Size = new System.Drawing.Size(512, 400);
-            this.richTextBoxOut.TabIndex = 0;
-            this.richTextBoxOut.Text = "";
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // Form1
             // 
@@ -133,12 +149,13 @@ namespace ConcatTXT
 
         private System.Windows.Forms.Panel panelInput;
         private System.Windows.Forms.Button btnStart;
-        private System.Windows.Forms.TextBox textBoxFPath;
+        private System.Windows.Forms.TextBox textBoxPath;
         private System.Windows.Forms.Label labelPath;
         private System.Windows.Forms.Panel panelOutput;
         private System.Windows.Forms.RichTextBox richTextBoxOut;
         private System.Windows.Forms.Button btnClearAll;
         private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.RichTextBox richTextBoxLog;
     }
 }
 
--- ConcatTXT/Form1.cs
+++ ConcatTXT/Form1.cs
@@ -7,11 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+
 
 namespace ConcatTXT
 {
     public partial class Form1 : Form
     {
+        private string rootPath; // для пути до папки
+        private List<string> listPaths = new List<string>(); // для путей txt файлов
+        private string rootPathOut; // для пути до выходного файла
+
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +26,124 @@ namespace ConcatTXT
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            rootPath = textBoxPath.Text;
+            // определяем путь выходного файла
+            rootPathOut = rootPath + "\\output567.txt";
+
+            // если есть директория по указанному пути
+            if (Directory.Exists(rootPath))
+            {
+                richTextBoxLog.Text += "\nПапка существует.";
+
+                btnClearAll.Enabled = true;
+                btnStart.Enabled = false;
+
+                // вызываем рекурсивную функцию
+                FindTxtFiles(rootPath, listPaths);
+            }
+            else
+            {
+                // иначе выводим проблему в label
+                richTextBoxLog.Text += "\nПапка не найдена.";
+            }
+
+            if (listPaths.Count > 0)
+            {
+                richTextBoxLog.Text += "\nТекстовые файлы найдены.";
+                // сортируем лист по алфавиту (в листе указаны полные пути, начиная с диска)
+                listPaths.Sort();
+
+                // выводим массив в текстовое поле
+                foreach (string str in listPaths)
+                {
+                    richTextBoxOut.Text += str + "\n";
+                }
+                // вызываем функцию для объединения файлов в один файл
+                ConcatTxtFiles(listPaths, rootPath);
+            }
+            else
+            {
+                richTextBoxLog.Text += "\nТекстовые файлы не найдены.";
+            }
+            if (File.Exists(rootPathOut))
+                richTextBoxLog.Text += "\nВыходной файл сформирован."
+                                       + "\nПуть: " + rootPathOut;
+
+        }
+        // функция принимает на вход путь до папки, в которой ищем текстовики,
+        // и лист для записи путей
+        private void FindTxtFiles(string path, List<string> listOut)
+        {
+            // добавляем каждый найденный файл в лист
+            foreach(string filepath in Directory.GetFiles(path))
+            {
+                listOut.Add(filepath);
+            }
+            // для каждой найденной папки вызываем эту же функцию
+            foreach(string subpath in Directory.GetDirectories(path))
+            {
+                FindTxtFiles(subpath, listOut);
+            }
+        }
+
+        // функция объединяет найденные ранее файлы в один файл
+        private void ConcatTxtFiles(List<string> txtPaths, string pathOut)
+        {
+            // создаем писаря для нового файла по пути пользователя
+            StreamWriter writer = new StreamWriter(rootPath + "\\output567.txt", false);
+            try
+            {
+                // для каждого пути из собранного листа
+                foreach (string path in txtPaths)
+                {
+                    // используем чтеца
+                    using (StreamReader reader = new StreamReader(path))
+                    {
+                        // читаем файл до конца
+                        string text = reader.ReadToEnd();
+                        // и передаем писарю
+                        writer.WriteLine(text);
+                    }
+                }
+            }
+            catch (IOException)
+            {
+                richTextBoxLog.Text += "Не удается записать или прочитать выходной файл.\n"
+                                        +"Убедитесь, что отсутствует файл по пути :\n"
+                                        +rootPathOut
+                                        + "и не используются любые файлы по пути :\n"
+                                        + rootPath;
+            }
+            finally
+            {
+                writer.Close();
+            }
+        }
+
+        // Удаляем файл и очищаем все переменные и поля
+        private void btnClearAll_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(rootPathOut)) File.Delete(rootPath + "\\output567.txt");
+
+            rootPath = "";
+            rootPathOut = "";
+            listPaths.Clear();
+
+            textBoxPath.Text = "";
+            richTextBoxOut.Text = "";
+            richTextBoxLog.Text = "Состояние :";
+
+            btnStart.Enabled = true;
+            btnClearAll.Enabled = false;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
